5da0ac93e1e7d8f92a5f9125e97ecb7cc651762b
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-02-11 15:07:29 +0200

    float double

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -14,4 +14,36 @@ public class Main {
         System.out.println("Busted MIN value = " + (myMinIntValue - 1));
 
         int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Bute Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Bute Maximum Value = " + myMaxShortValue);
+        
+        long myLongValue = 10000000000000000L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Bute Maximum Value = " + myMaxLongValue);
+        long bingLong = 2_147_483_647_234L;
+        System.out.println(bingLong);
+
+        short bigShort = 32767;
+        int myTotal = (myMinIntValue / 2);
+        byte myNewByteValue = (byte)(myMinByteValue / 2);
+        short myNewShortValue = (short)(myMinShortValue / 2);
+
+        byte myByte = 54;
+        short myShort = 7544;
+        int myInt = 7657455;
+        long myLong = 50000L + ((myByte + myShort + myInt) * 10L);
+        System.out.println(myLong);
+        short shortTotal = (short)(1000 + 10 * (myByte + myShort + myInt));
+        System.out.println(shortTotal);
+    }
 }
