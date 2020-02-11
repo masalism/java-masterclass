5433169d84dab9bdab40cb54614efc6cdfc6b3ea
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-02-11 14:22:38 +0200

    int

---
+++ ByteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10_000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
+}
