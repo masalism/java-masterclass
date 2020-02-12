1404ac6e4609d148bf4eef0a4c1524eb406b7e60
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-02-12 15:22:04 +0200

    more basics

---
+++ 2 more basics/KeywordsAndExpressions/src/mantas/company/Main.java
@@ -0,0 +1,29 @@
+package mantas.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is = 1.609344 km
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+        if(highScore== 50) {
+            System.out.println("This is an expression");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
