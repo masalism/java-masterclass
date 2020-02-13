4593fdcb3a0a9b5f3779467bd360b9d5349541f9
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-02-13 14:26:41 +0200

    methods

--- 2 more basics/Methods/src/mantas/company/Main.java
+++ 2 more basics/Methods/src/mantas/company/Main.java
@@ -8,22 +8,66 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score: " + highScore);
 
         gameOver = true;
-        score = 800;
-        levelCompleted = 5;
-        bonus = 100;
-        
-        calculateScore(true, 10000, 8, 200);
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score: " + highScore);
+
+        String playerName = "Mantas";
+        int otherScore = calculateHighScorePosition(1500);
+        displayHighScorePosition(playerName, otherScore);
+
+        playerName = "Darius";
+        otherScore = calculateHighScorePosition(900);
+        displayHighScorePosition(playerName, otherScore);
+
+        playerName = "Petras";
+        otherScore = calculateHighScorePosition(400);
+        displayHighScorePosition(playerName, otherScore);
+
+        playerName = "Andrius";
+        otherScore = calculateHighScorePosition(50);
+        displayHighScorePosition(playerName, otherScore);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score " + finalScore);
+            finalScore += 2000;
+            return finalScore;
         }
+        return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed to get into position " + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+//        if (score >= 1000) {
+//            return 1;
+//        } else if (score >= 500) {
+//            return 2;
+//        } else if (score >= 100) {
+//            return 3;
+//        }
+//        return  4;
+
+        int position = 4; // assuming position 4 will be returned
+
+        if (score >= 1000) {
+            position = 1;
+        } else if (score >= 500) {
+            position =  2;
+        } else if (score >= 100) {
+            position = 3;
+        }
+        return position;
     }
 }
