899a0332bed8e223d161b28155ffdf05bafb247c
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-02-20 12:31:32 +0200

    switch

---
+++ 3 statements/Switch/src/mantas/company/Main.java
@@ -0,0 +1,63 @@
+package mantas.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int value = 3;
+        if (value == 1) {
+            System.out.println("Value was 1");
+        } else if (value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
+
+        int switchValue = 3;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, 4 or 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+            default:
+                System.out.println("Was not 1, 2, 3, 4 or 5");
+                break;
+        }
+
+        char charValue = 'f';
+
+        switch (charValue) {
+            case 'A':
+                System.out.println("A");
+                break;
+            case 'B':
+                System.out.println("B");
+                break;
+            case 'C': case 'D': case 'E':
+                System.out.println("c d or e");
+                break;
+            default:
+                System.out.println("Nothing was found");
+                break;
+        }
+
+        String month = "June";
+
+        switch (month.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Nothing");
+                break;
+        }
+    }
+}
